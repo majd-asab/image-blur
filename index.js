23e05cb2dd1112bb50357b05922a27209a0594e6
majd-kho <kho.majds@gmail.com>
2018-01-31 23:56:06 -0500

    fixed indentation

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ var Jimp = require("jimp");
 // blured img and crop the top and bottom extra parts.
 Jimp.read("img1.jpg", function (err, img) {
  if (err) throw err;
-	let x = img.bitmap.width;
-	let y = img.bitmap.height;
+ let x = img.bitmap.width;
+ let y = img.bitmap.height;
  img.scale(2)  
-			 .blur(10)						       
-    .quality(100) 		               
-	new Jimp("img1.jpg",function(err,img2){
-		img.composite(img2,x/2,y/2)
-				 .crop(0,  // x-coordinate
-										y/2, // y-coordinate
-										x*2, // full width of doubled image
-											y)	 // full height of original image
-				 .write("blured-double-and-on-top.jpg");
-		});
+ .blur(10)             
+ .quality(100)                  
+ new Jimp("img1.jpg",function(err,img2){
+  img.composite(img2,x/2,y/2)
+     .crop(0,  // x-coordinate
+          y/2, // y-coordinate
+          x*2, // full width of doubled image
+           y)  // full height of original image
+     .write("blured-double-and-on-top.jpg");
+    });
 });
